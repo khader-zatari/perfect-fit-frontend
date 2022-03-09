e62f5c70bca96fb95922bbf5cef74414a5f1841e
khader-zatari <khaderzatari@gmail.com>
2022-03-09 13:08:50 +0200

    single page update

--- Screens/Products/SingleProduct.js
+++ Screens/Products/SingleProduct.js
@@ -1,4 +1,4 @@
-import { Center } from "native-base";
+import { Button, Center } from "native-base";
 import React from "react";
 import { Text, View, ScrollView, StyleSheet, Image, Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
@@ -6,7 +6,20 @@ const SingleProduct = () => {
     return (
         <ScrollView>
             <View style={styles.container}>
-                <Image style={styles.imageContainer} resizeMode="contain" source={require("../../assets/1638427112t-shirt-png.png")} />
+                <View style={styles.imageContainer}>
+                    <Image style={styles.image} resizeMode="contain" source={require("../../assets/1638427112t-shirt-png.png")} />
+                </View>
+                <View style={styles.secondPart}>
+                    <Text>black T-shirt</Text>
+                    <View style={styles.buttonContainer}>
+                        <Button style={styles.button} size="16">
+                            Add to Cart
+                        </Button>
+                    </View>
+                    <View style={styles.descriptionContainer}>
+                        <Text>flasjdflkasjd;flkjas;ldkfjas;ldkfj;lsakdjf;laskdjf;laskdjf;salkdjf;salkdjfaslkdjflkasdjf;lkasjdf;lkjsad;lkfj;</Text>
+                    </View>
+                </View>
             </View>
         </ScrollView>
     );
@@ -14,13 +27,30 @@ const SingleProduct = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        height: height / 2,
-        width: width,
     },
     imageContainer: {
         flex: 1,
+        height: height / 2,
+        width: width,
+    },
+    image: {
+        flex: 1,
         width: "100%",
+        height: "100%",
         resizeMode: "contain",
+    },
+    secondPart: {
+        width: width,
+        height: height,
+    },
+    buttonContainer: {
+        alignItems: "center",
+
+        justifyContent: "center",
+    },
+    button: { width: "90%" },
+    descriptionContainer: {
+        padding: 20,
     },
 });
 export default SingleProduct;
