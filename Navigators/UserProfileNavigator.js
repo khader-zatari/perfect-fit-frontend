9f51661458baeb92adac8c777e0e81d9389510cf
khader-zatari <khaderzatari@gmail.com>
2022-03-13 13:01:11 +0200

    LogIn is done

--- Navigators/UserProfileNavigator.js
+++ Navigators/UserProfileNavigator.js
@@ -2,13 +2,13 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import UserProfile from "../Screens/User/UserProfile";
 import SignUp from "../Screens/User/SignUp";
-
+import LogIn from "../Screens/User/LogIn";
 const Stack = createNativeStackNavigator();
 
 const UserProfileNavigator = () => {
     return (
         <Stack.Navigator
-            initialRouteName="SignUp"
+            initialRouteName="LogIn"
             screenOptions={{
                 contentStyle: {
                     backgroundColor: "#FFFFFF",
@@ -17,6 +17,7 @@ const UserProfileNavigator = () => {
         >
             <Stack.Screen name="Userpage" component={UserProfile} />
             <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="LogIn" component={LogIn} />
         </Stack.Navigator>
     );
 };
