29c3ec826d4b438b11a8449a63c975740b6d7084
khader-zatari <khaderzatari@gmail.com>
2022-03-30 21:00:29 +0300

    bug fix

--- Navigators/UserProfileNavigator.js
+++ Navigators/UserProfileNavigator.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import UserProfile from "../Screens/User/UserProfile";
 import SignUp from "../Screens/User/SignUp";
 import LogIn from "../Screens/User/LogIn";
+import ShippingAddress from "../Screens/User/ShippingAddress";
 const Stack = createNativeStackNavigator();
 
 const UserProfileNavigator = () => {
@@ -18,6 +19,7 @@ const UserProfileNavigator = () => {
             <Stack.Screen name="Userpage" component={UserProfile} />
             <Stack.Screen name="SignUp" component={SignUp} />
             <Stack.Screen name="LogIn" component={LogIn} />
+            <Stack.Screen name="ShippingAddress" component={ShippingAddress} />
         </Stack.Navigator>
     );
 };
--- Screens/User/UserProfile.js
+++ Screens/User/UserProfile.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { Text, View } from "react-native";
-
-const UserProfile = () => {
+import { Text, View, StyleSheet } from "react-native";
+import { Button } from "native-base";
+const UserProfile = (props) => {
     return (
-        <View>
-            <Text>what the hell</Text>
+        <View style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <Button style={styles.button} size="16" onPress={() => {props.navigation.navigate("ShippingAddress")}}>
+                    Shipping Address
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button style={styles.button} size="16" onPress={() => {}}>
+                    Buy
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button style={styles.button} size="16" onPress={() => {}}>
+                    Buy
+                </Button>
+            </View>
         </View>
     );
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        height: "60%",
+    },
+    buttonContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    button: { width: "90%" },
+    descriptionContainer: {
+        flex: 1,
+        padding: 20,
+    },
+});
 
 export default UserProfile;
