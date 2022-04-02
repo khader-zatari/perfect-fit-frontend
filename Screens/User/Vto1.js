45d4ac24eb5917e6d8501ca3eca037b5667571c9
khader-zatari <khaderzatari@gmail.com>
2022-04-02 17:43:24 +0300

    upload photo to aws S3

--- Screens/User/Vto1.js
+++ Screens/User/Vto1.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from "react";
 import { Text, View, StyleSheet, TouchableOpacity, Alert, Image } from "react-native";
 import { Button } from "native-base";
 import * as ImagePicker from "expo-image-picker";
+import { RNS3 } from "react-native-aws3";
+var secret = require("../../Shared/Secret");
 
 const Vto1 = (props) => {
     const [image, setImage] = useState(null);
@@ -18,8 +20,54 @@ const Vto1 = (props) => {
         console.log(result);
 
         if (!result.cancelled) {
+            uploadImageToS3(result)
+                .then(() => {
+                    console.log("sucsess");
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
             setImage(result.uri);
         }
+    };
+    const uploadImageToS3 = async (image) => {
+        const options = {
+            keyPrefix: "uploads/",
+            bucket: "pfakhader",
+            region: "eu-west-1",
+            accessKey: secret.ACCESSKEY,
+            secretKey: secret.SECRETKEY,
+            successActionStatus: 201,
+        };
+        const fileName = image.uri.replace(/^.*[\\\/]/, "");
+        const file = {
+            uri: image.uri,
+            name: fileName,
+            type: image.uri.substring(image.uri.lastIndexOf(".") + 1), //extracting filename from image path,
+        };
+        return new Promise((resolve, reject) => {
+            RNS3.put(file, options)
+                .then((res) => {
+                    if (res.status === 201) {
+                        const { postResponse } = res.body;
+                        resolve({
+                            src: postResponse.location,
+                        });
+                    } else {
+                        console.log("error uploading to s3", res);
+                    }
+                })
+                .catch((err) => {
+                    console.log("error uploading to s3", err);
+                    reject(err);
+                });
+        });
+    };
+    const uploadAllImages = (image) => {
+        let promises = [];
+        image.map((image, i) => {
+            promises.push(uploadImageToS3(image));
+        });
     };
 
     return (
