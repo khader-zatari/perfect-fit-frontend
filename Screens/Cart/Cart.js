409de5e504ce280584cb9db9faeed53c0b5d836f
khader-zatari <khaderzatari@gmail.com>
2022-04-08 15:53:35 +0300

    display message when the cart is empty

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -9,31 +9,45 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const { height } = Dimensions.get("window");
 
 const Cart = (props) => {
+    var total = 0;
+
+    props.cartItems.forEach((cart) => {
+        return (total += cart.product.price);
+    });
     return (
         <SafeAreaView edges={["top", "left", "right"]}>
             <ScrollView>
-                <View style={styles.Container}>
-                    {props.cartItems.map((item) => {
-                        //return <ProductCartList item={item} />;
-                        return <ProductCartCard item={item} />;
-                    })}
-                </View>
-                <View style={styles.priceAndButton}>
-                    <View style={styles.price}>
-                        <Text>Total 200$</Text>
+                {props.cartItems.length ? (
+                    <View>
+                        <View style={styles.Container}>
+                            {props.cartItems.map((item) => {
+                                //return <ProductCartList item={item} />;
+                                return <ProductCartCard item={item} />;
+                            })}
+                        </View>
+                        <View style={styles.priceAndButton}>
+                            <View style={styles.price}>
+                                <Text>Total 200$</Text>
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                <Button
+                                    style={styles.button}
+                                    size="16"
+                                    onPress={() => {
+                                        props.clearCart();
+                                    }}
+                                >
+                                    Buy with one click
+                                </Button>
+                            </View>
+                        </View>
                     </View>
-                    <View style={styles.buttonContainer}>
-                        <Button
-                            style={styles.button}
-                            size="16"
-                            onPress={() => {
-                                props.clearCart();
-                            }}
-                        >
-                            Buy with one click
-                        </Button>
+                ) : (
+                    <View style={styles.emptyCart}>
+                        <Text>Looks like your cart is empty</Text>
+                        <Text>Add products to your cart to get started</Text>
                     </View>
-                </View>
+                )}
             </ScrollView>
         </SafeAreaView>
     );
@@ -76,6 +90,13 @@ const styles = StyleSheet.create({
     },
     price: {
         alignSelf: "center",
+    },
+    emptyCart: {
+        flex: 1,
+        width: "100%",
+        height: height,
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
 
