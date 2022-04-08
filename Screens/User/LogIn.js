b6e70baf4eaabd7e5da0f83439dcc8e812d1a119
khader-zatari <khaderzatari@gmail.com>
2022-04-08 16:31:36 +0300

    Login is updated

--- Screens/User/LogIn.js
+++ Screens/User/LogIn.js
@@ -1,41 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "native-base";
 import { Text, View, StyleSheet, ScrollView, Dimensions, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const { height, width } = Dimensions.get("window");
 const LogIn = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = () => {
+        const user = {
+            email,
+            password,
+        };
+    };
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={styles.headerContainer}>
-                    <Text style={styles.headerText}>Login</Text>
-                </View>
-                <View>
-                    <View style={styles.input}>
-                        <Text style={{ paddingVertical: 10, fontSize: 11 }}>Email</Text>
-                        <TextInput style={styles.TextInput} placeholder="Email" id="email" name="email" />
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.headerContainer}>
+                        <Text style={styles.headerText}>Login</Text>
                     </View>
-                    <View style={styles.input}>
-                        <Text style={{ paddingVertical: 10, fontSize: 11 }}>Password</Text>
-                        <TextInput style={styles.TextInput} placeholder="Password" id="password" name="password" />
+                    <View>
+                        <View style={styles.input}>
+                            <Text style={{ paddingVertical: 10, fontSize: 11 }}>Email</Text>
+                            <TextInput style={styles.TextInput} placeholder="Email" id="email" name="email" onChangeText={(text) => setEmail(text.toLowerCase())} />
+                        </View>
+                        <View style={styles.input}>
+                            <Text style={{ paddingVertical: 10, fontSize: 11 }}>Password</Text>
+                            <TextInput style={styles.TextInput} placeholder="Password" id="password" name="password" onChangeText={(text) => setPassword(text)} />
+                        </View>
+                    </View>
+                    <View style={styles.secondPart}>
+                        <View style={styles.buttonContainer}>
+                            <Button
+                                style={styles.button}
+                                size="12"
+                                onPress={() => {
+                                    //check email and password nav to user page else to error
+                                    props.navigation.navigate("Userpage");
+                                }}
+                            >
+                                <Text style={{ color: "#FFF", fontSize: 16, fontWeight: "bold" }}>LOGIN</Text>
+                            </Button>
+
+                            <Button
+                                style={styles.button}
+                                size="12"
+                                onPress={() => {
+                                    props.navigation.navigate("SignUp");
+                                }}
+                            >
+                                <Text style={{ color: "#FFF", fontSize: 16, fontWeight: "bold" }}>REGISTER</Text>
+                            </Button>
+                        </View>
                     </View>
                 </View>
-                <View style={styles.secondPart}>
-                    <View style={styles.buttonContainer}>
-                        <Button style={styles.button} size="12" onPress={() => { props.navigation.navigate("Userpage");}}>
-                            <Text style={{ color: "#FFF", fontSize: 16, fontWeight: "bold" }}>LOGIN</Text>
-                        </Button>
-                    </View>
-                </View>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         width: "90%",
-        height: height / 2,
+        height: height / 1.5,
         alignSelf: "center",
     },
     headerText: {
@@ -62,7 +92,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         marginVertical: "10%",
     },
-    button: { width: "100%" },
+    button: { width: "100%", marginVertical: "2%" },
     secondPart: {},
 });
 
